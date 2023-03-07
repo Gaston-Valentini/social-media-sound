8ded8473b749126997e8fa79a66a3416b7072e09
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-03-07 18:25:08 +0100

    about añadido

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 
 import Navbar from "./Components/Navbar/Navbar"
 import Header from "./Components/Header/Header"
+import About from "./Components/About/About"
 
 export default function App(params) {
     return (
         <div>
             <Navbar/>
             <Header/>
+            <About/>
         </div>
     )
 }
---
+++ client/src/Components/About/About.jsx
@@ -0,0 +1,48 @@
+import style from "./About.module.css"
+
+import image1 from "../../Images/header.png"
+import image2 from "../../Images/headerMic1.png"
+import image3 from "../../Images/headerMic2.png"
+
+export default function About() {
+    return (
+        <div className={style.about}>
+            <div className={style.aboutTitles}>
+                <p className={style.aboutTitlesTitle}>Qué hacemos?</p>
+                <p className={style.aboutTitlesSubtitle}>Servicios</p>
+            </div>
+            <div className={style.aboutCards}>
+                <div className={style.aboutCardsCard}>
+                    <div className={style.aboutCardsCardImage}>
+                        <img src={image1} alt={"image1"}></img>
+                    </div>
+                    <div className={style.aboutCardsCardData}>
+                        <p className={style.aboutCardsCardDataTitle}>Mobile Studio</p>
+                        <p className={style.aboutCardsCardDataSubtitle}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, nisi. Neque minima placeat porro, earum temporibus at a eaque nemo, consequuntur commodi voluptate ad corporis voluptatibus et ipsa modi provident.</p>
+                        <button className={style.aboutCardsCardDataButton}>Saber más</button>
+                    </div>
+                </div>
+                <div className={style.aboutCardsCard}>
+                    <div className={style.aboutCardsCardImage}>
+                        <img src={image2} alt={"image2"}></img>
+                    </div>
+                    <div className={style.aboutCardsCardData}>
+                        <p className={style.aboutCardsCardDataTitle}>Mobile Studio</p>
+                        <p className={style.aboutCardsCardDataSubtitle}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, nisi. Neque minima placeat porro, earum temporibus at a eaque nemo, consequuntur commodi voluptate ad corporis voluptatibus et ipsa modi provident.</p>
+                        <button className={style.aboutCardsCardDataButton}>Saber más</button>
+                    </div>
+                </div>
+                <div className={style.aboutCardsCard}>
+                    <div className={style.aboutCardsCardImage}>
+                        <img src={image3} alt={"image3"}></img>
+                    </div>
+                    <div className={style.aboutCardsCardData}>
+                        <p className={style.aboutCardsCardDataTitle}>Mobile Studio</p>
+                        <p className={style.aboutCardsCardDataSubtitle}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, nisi. Neque minima placeat porro, earum temporibus at a eaque nemo, consequuntur commodi voluptate ad corporis voluptatibus et ipsa modi provident.</p>
+                        <button className={style.aboutCardsCardDataButton}>Saber más</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
