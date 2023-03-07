bc0c3fee649332dc999c5e870c435db98c4b622f
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-03-07 17:04:28 +0100

    header añadido

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import style from "./Header.module.css"
+
+import logo from "../../Images/logo.png"
+
 export default function Header(params) {
     return (
-        <div>
-            Header
+        <div className={style.header}>
+            <div className={style.headerData}>
+                <div className={style.headerDataImageContainer}>
+                    <img src={logo} alt={"logo"} className={style.headerDataImage}></img>
+                </div>
+                <p className={style.headerDataText}>Música hecha a tu medida con los mejores profesionales</p>
+                <button className={style.headerDataButton}>Comenzemos!</button>
+            </div>
         </div>
     )
 }
