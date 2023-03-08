3867d8e3f14c429619cc681f57d1495c5436810e
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-03-08 17:04:47 +0100

    slider

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,4 +1,4 @@
-import style from "./About.module.css"
+import  "./About.css"
 
 import image1 from "../../Images/header.png"
 import image2 from "../../Images/headerMic1.png"
@@ -6,20 +6,39 @@ import image3 from "../../Images/headerMic2.png"
 
 export default function About() {
 
-    
-
     return (
-        <div className={style.about}>
-            <div className={style.aboutContainer}>
-                <ul className={style.aboutContainerSlider}>
-                    <li className={style.aboutContainerSliderSlide} id="slide1"><img src={image1}></img></li>
-                    <li className={style.aboutContainerSliderSlide} id="slide1"><img src={image2}></img></li>
-                    <li className={style.aboutContainerSliderSlide} id="slide1"><img src={image3}></img></li>
+        <div className="about">
+            <div className="aboutContainer">
+                <ul className="aboutContainerSlider">
+                    <li className="aboutContainerSliderSlide" id="slide1">
+                        <div className="aboutContainerSliderSlideImage"><img src={image1} alt={"image1"}></img></div>
+                        <div className="aboutContainerSliderSlideData">
+                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
+                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
+                            <span className="aboutContainerSliderSlideDataPrice">Price</span>
+                        </div>
+                    </li>
+                    <li className="aboutContainerSliderSlide" id="slide2">
+                        <div className="aboutContainerSliderSlideImage"><img src={image2} alt={"image2"}></img></div>
+                        <div className="aboutContainerSliderSlideData">
+                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
+                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
+                            <p className="aboutContainerSliderSlideDataPrice">Price</p>
+                        </div>
+                    </li>
+                    <li className="aboutContainerSliderSlide" id="slide3">
+                        <div className="aboutContainerSliderSlideImage"><img src={image3} alt={"image3"}></img></div>
+                        <div className="aboutContainerSliderSlideData">
+                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
+                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
+                            <p className="aboutContainerSliderSlideDataPrice">Price</p>
+                        </div>
+                    </li>
                 </ul>
-                <ul className={style.aboutContainerNavbar}>
-                    <li className={style.aboutContainerNavbarLink}><a href="#slide1">1</a></li>
-                    <li className={style.aboutContainerNavbarLink}><a href="#slide2">2</a></li>
-                    <li className={style.aboutContainerNavbarLink}><a href="#slide3">3</a></li>
+                <ul className="aboutContainerNavbar">
+                    <li className="aboutContainerNavbarLink"><a href="#slide1">.</a></li>
+                    <li className="aboutContainerNavbarLink"><a href="#slide2">.</a></li>
+                    <li className="aboutContainerNavbarLink"><a href="#slide3">.</a></li>
                 </ul>
             </div>
         </div>
