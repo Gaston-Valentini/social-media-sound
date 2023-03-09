193690bd17501a6c206f7b46504bcc5d81701402
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-03-09 14:48:40 +0100

    nada

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,49 +1,68 @@
 import  "./About.css"
 
+import { useState, useEffect } from "react"
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs"
+
 import image1 from "../../Images/header.png"
 import image2 from "../../Images/headerMic1.png"
 import image3 from "../../Images/headerMic2.png"
 
-
 export default function About() {
+    
+    const [position, setPosition] = useState(0)
+
+    const images = [image1, image2, image3]
+    const [image, setImage] = useState(images[position])
+
+    const titles = ["Título 1", "Título 2", "Título 3"]
+    const [title, setTitle] = useState(titles[position])
+    
+    const texts = ["Texto 1", "Texto 2", "Texto 3"]
+    const [text, setText] = useState(texts[position])
+    
+    const prices = ["Precio 1", "Precio 2", "Precio 3"]
+    const [price, setPrice] = useState(prices[position])
+
+    useEffect(() => {
+        setImage(images[position])
+        setTitle(titles[position])
+        setText(texts[position])
+        setPrice(prices[position])
+    }, [position])
+
+    const onLeft = () => {
+        if (position === 0) {
+            setPosition(2)
+        } else {
+            setPosition(position - 1)
+        }
+    }
+
+    const onRight = () => {
+        if (position === 2) {
+            setPosition(0)
+        } else {
+            setPosition(position + 1)
+        }
+    }
+
     return (
-        <div id ="services" className="about">
-            <div className="aboutTitles">
-                <p className="aboutTitlesTitle">Servicios</p>
-                <p className="aboutTitlesSubtitle">Qué hacemos?</p>
+        <div className="about">
+            <div className="titles">
+                <p className="titlesTitle">Formulario</p>
+                <p className="titlesSubtitle">Háblanos de tu video</p>
             </div>
-            <div className="aboutContainer">
-                <ul className="aboutContainerSlider">
-                    <li className="aboutContainerSliderSlide" id="slide1">
-                        <div className="aboutContainerSliderSlideImage"><img src={image1} alt={"image1"}></img></div>
-                        <div className="aboutContainerSliderSlideData">
-                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
-                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
-                            <span className="aboutContainerSliderSlideDataPrice">Price</span>
-                        </div>
-                    </li>
-                    <li className="aboutContainerSliderSlide" id="slide2">
-                        <div className="aboutContainerSliderSlideImage"><img src={image2} alt={"image2"}></img></div>
-                        <div className="aboutContainerSliderSlideData">
-                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
-                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
-                            <p className="aboutContainerSliderSlideDataPrice">Price</p>
-                        </div>
-                    </li>
-                    <li className="aboutContainerSliderSlide" id="slide3">
-                        <div className="aboutContainerSliderSlideImage"><img src={image3} alt={"image3"}></img></div>
-                        <div className="aboutContainerSliderSlideData">
-                            <p className="aboutContainerSliderSlideDataTitle">Lorem ipsum dolor sit.</p>
-                            <p className="aboutContainerSliderSlideDataSubtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi quia ipsum, necessitatibus deserunt sint!</p>
-                            <p className="aboutContainerSliderSlideDataPrice">Price</p>
-                        </div>
-                    </li>
-                </ul>
-                <ul className="aboutContainerNavbar">
-                    <li className="aboutContainerNavbarLink"><a href="#slide1">A</a></li>
-                    <li className="aboutContainerNavbarLink"><a href="#slide2">B</a></li>
-                    <li className="aboutContainerNavbarLink"><a href="#slide3">C</a></li>
-                </ul>
+            <div className="aboutSlider">
+                <div className="aboutSliderImage"><img src={image} alt="imageSlider"/></div>
+                <div className="aboutSliderData">
+                    <p className="aboutSliderDataTitle">{title}</p>
+                    <p className="aboutSliderDataSubtitle">{text}</p>
+                    <button className="aboutSliderDataButton">{price}</button>
+                </div>
+            </div>
+            <div className="aboutNavbar">
+                <BsFillArrowLeftCircleFill className="aboutNavbarButton" onClick={onLeft}/>
+                <BsFillArrowRightCircleFill className="aboutNavbarButton" onClick={onRight}/>
             </div>
         </div>
     )
