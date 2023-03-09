245cef87917370d4da27c432d2941e2dd5a4b5c9
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-03-09 18:58:47 +0100

    finish

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import style from "./Header.module.css"
 
+import { Link } from "react-scroll"
+
 import logo from "../../Images/logo.png"
 
 export default function Header(params) {
@@ -11,7 +13,7 @@ export default function Header(params) {
                 </div>
                 <p className={style.headerDataText}>Muéstranos tu video y dinos cómo te gustaría que suene</p>
                 <p className={style.headerDataInfo}>Pide presupuesto sin compromiso, no vas a encontrar un precio mejor. Además, si nos gusta tu proyecto lo hacemos gratis, porque nos gusta participar y ayudar a que los buenos proyectos vean la luz.</p>
-                <button className={style.headerDataButton}>Mejorar mi audio!</button>
+                <Link to="contact" className={style.headerDataButton} spy={true} smooth={true} offset={50} duration={500}>Mejorar mi audio!</Link>
             </div>
         </div>
     )
